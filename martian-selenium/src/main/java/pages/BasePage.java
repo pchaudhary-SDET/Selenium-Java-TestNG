d3318d923b054de55aef3e4c00022ad483471aa1
pchaudhary-SDET <job2priyanka@gmail.com>
2025-08-29 10:51:35 -0500

    Added base Page

---
+++ martian-selenium/src/main/java/pages/BasePage.java
@@ -0,0 +1,44 @@
+package pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import utils.ConfigReader;
+
+public class BasePage {
+    protected WebDriver driver;
+    private WebDriverWait wait;
+
+
+    public BasePage(WebDriver driver){
+        this.driver=driver;
+        int timeout = Integer.parseInt(ConfigReader.getProperty("EXPLICIT_WAIT"));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+    }
+    protected void click(By locator){
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    protected void type(By locator, String text){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(text);;
+    }
+
+    protected String getText(By locator){
+       return wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).getText();
+    }
+
+    protected Boolean isDisplayed(By locator){
+        try {
+            return wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).isDisplayed();
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            return false;
+        }
+    }
+    
+}
